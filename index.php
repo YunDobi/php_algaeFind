497257dc5f02d26783200b14bc32b2f5aea05a4e
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-07-15 20:58:16 -0700

    second commit for button

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
 <!DOCTYPE html>
 <html>
-      
-<?php
-include("header.php")
-?>
- <h1 style="color:green;">
-      AlgeaFind
-  </h1>
-  
-<body style="text-align:center;">
-      
-    <?php
-        if(array_key_exists('button1', $_POST)) {
-          include("first.html");
-        }
-        else if(array_key_exists('button2', $_POST)) {
-          include("second.html");
-        }
-    ?>
-  
-    <form method="post" action="index.php">
-        <input type="submit" name="button1" class="button" value="Bone Loss" />
+  <link rel="stylesheet" href="style.css">
+  <body>
+    <div class= "leftBox">
+      <h2>Algea Finder</h2>
+    </div>
+
+  <div class="contentBox">
+    <form method="post" action="index.php" class="buttonList">
+      <input type="submit" name="button1" class="button" value="Bone Loss" />
           
         <input type="submit" name="button2" class="button" value="Bone healing" />
 
         <input type="submit" name="button3" class="button" value="Bone boost" />
     </form>
+
+    <?php
+          if(array_key_exists('button1', $_POST)) {
+            include("first.php");
+          }
+          else if(array_key_exists('button2', $_POST)) {
+            include("second.php");
+          }
+          else if(array_key_exists('button3', $_POST)) {
+            include("third.php");
+          } else {
+            echo "<h1>Welcome and choose your health!</h1> <br>";
+          }
+      ?>
+  </div>
+
+  
 </body>
   
 </html>
